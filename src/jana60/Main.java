0992e165ab62345fdcbfdd09364f5ca76908f691
dacunzogaetano <dacunzogaetano@gmail.com>
2022-06-29 23:37:09 +0200

    esercizio liste c

---
+++ src/jana60/Main.java
@@ -0,0 +1,57 @@
+package jana60;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+	
+	/* Creare una classe Main con metodo main in cui implementare il seguente programma:
+		Creare una lista dei desideri vuota e chiedere all’utente di aggiungere i suoi desideri uno alla volta, fino a che sceglie di fermarsi.
+		Ogni volta che viene aggiunto un desiderio stampare a video quanti desideri sono contenuti nella lista in quel momento.
+		Al termine dell’inserimento stampare a video la lista ordinata.
+		…poi, visto che siamo buoni ma non buonissimi, possiamo anche mettere un limite a quanti desideri può inserire una persona */
+
+	public static void main(String[] args) {
+		//istanza dell'arraylist e dello scanner
+		ArrayList<String> listaDesideri = new ArrayList<>();
+		Scanner scelta = new Scanner(System.in);
+		boolean sceltaValida = true;
+		
+		do {
+		System.out.println("Premi 1 per inserire un nuovo desiderio o 2 per uscire");
+		String inputScelta = scelta.nextLine();
+		switch(inputScelta) {
+		//scelta per aggiungere desideri
+		case "1":
+			System.out.println("Cosa vuoi aggiungere alla lista desideri?");
+			listaDesideri.add(scelta.nextLine()); 
+			System.out.println("La tua lista desideri è composta da  "+listaDesideri.size()+" desideri.");
+			sceltaValida = false;
+			break;
+		//scelta per non aggiungere più desideri
+		case "2":
+			System.out.println("Hai deciso di non aggiungere più nulla alla tua lista.");
+			sceltaValida = true;
+			break;
+		//scelta per chi non mette ne 1 ne 2
+		default:
+			System.out.println("Inserisci o 1 o 2");
+			sceltaValida = false;
+			break;
+		}
+		
+		} while (!sceltaValida && listaDesideri.size()<=10); //controllo che i desideri non siano più di 10
+		
+		//ordine alfabetico per i regali e stampa della lista
+		if(listaDesideri.size()>0) {
+		Collections.sort(listaDesideri);
+		System.out.println("La tua lista desideri ordinata: "+listaDesideri);
+		}
+		else {
+			System.out.println("Non hai inserito nemmeno un desiderio.");
+		}
+		scelta.close();
+	}
+
+}
